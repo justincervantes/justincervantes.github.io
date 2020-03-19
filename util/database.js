9ee7c68539e6c7f588550a1cefe7c0f371721e19
Justin Cervantes <cervantes.jfa@gmail.com>
2020-03-19 02:57:36 -0700

    Updating the database to a free service

--- util/database.js
+++ util/database.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2'); 
  
 const pool = mysql.createPool({  
-    host: 'localhost',  
-    user: 'root',  
-    database: 'peoplebook',  
-    password: '',
-    port: 3308
+    host: 'remotemysql.com',  
+    user: 'OrkgSPdd8n',  
+    database: 'OrkgSPdd8n',  
+    password: 'cBm7yGt0W2',
+    port: 3306
 });  
 
 module.exports = pool.promise(); 
